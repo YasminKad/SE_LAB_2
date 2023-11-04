f5b860b1993c9be99fb8fb0558bab7da148bbc11
yasminkadkhodaei <59218135+yasminkadkhodaei@users.noreply.github.com>
2023-11-04 19:27:46 +0330

    Add/Implement orderRegister method in PhoneOrderService class

--- untitled/src/Step_3_With_SOLID/PaymentServices/PhoneOrderService.java
+++ untitled/src/Step_3_With_SOLID/PaymentServices/PhoneOrderService.java
@@ -5,4 +5,9 @@ public class PhoneOrderService implements OrderService {
     public void orderPayment(int foodPrice){
         System.out.println("Phone Payment with Price : " + foodPrice + " Tomans!");
     }
+
+    @Override
+    public void orderRegister(String customerName){
+        System.out.println("on-site order registered for " + customerName);
+    }
 }
